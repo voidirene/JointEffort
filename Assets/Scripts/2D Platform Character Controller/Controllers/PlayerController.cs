af41e5639e753d5d29984961d9182ed9851728a2
Irene <daigoukaruna@gmail.com>
2023-02-20 16:16:02 +0200

    added input support for second player

--- Assets/Scripts/2D Platform Character Controller/Controllers/PlayerController.cs
+++ Assets/Scripts/2D Platform Character Controller/Controllers/PlayerController.cs
@@ -5,14 +5,18 @@ namespace Shinjingi
     [CreateAssetMenu(fileName = "PlayerController", menuName = "InputController/PlayerController")]
     public class PlayerController : InputController
     {
+        //edited to work with the unity input manager, in order to support a second player
+        [SerializeField] private string moveAxisName = "Horizontal";
+        [SerializeField] private string jumpAxisName = "Jump";
+
         public override bool RetrieveJumpInput()
         {
-            return Input.GetButtonDown("Jump");
+            return Input.GetButtonDown(jumpAxisName);
         }
 
         public override float RetrieveMoveInput()
         {
-            return Input.GetAxisRaw("Horizontal");
+            return Input.GetAxisRaw(moveAxisName);
         }
     }
 }
